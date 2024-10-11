73b336cbd281804123c39d9c0787167297cb2758
Nabil ork <99547322+Nabil-ork@users.noreply.github.com>
2024-10-11 14:20:56 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,43 +14,31 @@ class Footer extends Component {
   render() {
     const current_year = new Date().getFullYear();
     return (
-      <div id="section_footer">
-        <div className='text-center py-4'>
-          <Link href="https://1manstartup.com/privacy-policy" target="_blank" className='btn btn-link'>
-            Privacy
-          </Link>
-          <span>|</span>
-          <Link href="https://1manstartup.com/terms-and-conditions" target="_blank" className='btn btn-link'>
-            Terms
-          </Link>
-          <span>|</span>
-          <Link href="mailto:Roads Audio<dave@roadsaudio.com>" className='btn btn-link'>
-            Contact
-          </Link>
+      <div id="section_footer" class="bg-graident">
 
           <div className='mt-2 mb-3'>
-            <Link href="https://www.facebook.com/profile.php?id=61562204128726" target="_blank" className='social-icon mx-1'>
+            <Link href="https://www.facebook.com/" target="_blank" className='social-icon mx-1'>
               <IoLogoFacebook size={25}/>
             </Link>
 
-            <Link href="https://www.instagram.com/roadsaudio/" target="_blank" className='social-icon mx-1'>
+            <Link href="https://www.instagram.com/" target="_blank" className='social-icon mx-1'>
               <IoLogoInstagram size={25}/>
             </Link>
 
-            <Link href="https://www.linkedin.com/company/roads-audio/" target="_blank" className='social-icon mx-1'>
+            <Link href="https://www.linkedin.com/" target="_blank" className='social-icon mx-1'>
               <IoLogoLinkedin size={25}/>
             </Link>
 
-            <Link href="https://www.tiktok.com/@roadsaudio" target="_blank" className='social-icon mx-1'>
+            <Link href="https://www.tiktok.com/" target="_blank" className='social-icon mx-1'>
               <IoLogoTiktok size={25}/>
             </Link>
 
-            <Link href="https://www.youtube.com/@RoadsAudio" target="_blank" className='social-icon mx-1'>
+            <Link href="https://www.youtube.com/" target="_blank" className='social-icon mx-1'>
               <IoLogoYoutube size={25}/>
             </Link>
           </div>
 
-          <p>Copyright © {current_year}, 1ManStartup LLC. All Rights Reserved.</p>
+          <p>Copyright © {current_year}, Codes Waves. All Rights Reserved.</p>
         </div>
       </div>
     );
