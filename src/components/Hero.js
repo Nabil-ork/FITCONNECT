90602fcd9e3247bc6592ec77c16d618ce5233c97
Nabil ork <99547322+Nabil-ork@users.noreply.github.com>
2024-10-11 14:04:21 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,29 +20,7 @@ class Hero extends Component {
                   className="pr-3"
                 />
               </div>
-            <span className={styles.vLine1}>
-              <Image 
-                src="images/node-bright.svg" 
-                width={15} 
-                height={15} 
-                alt="Circle used for background style"
-                className={`${styles.node_sm1} d-none d-md-block`}
-              />
-            </span>
-            <span className={`${styles.vLine2} d-none d-md-block`}></span>
-            <span className={styles.vLine3}></span>
-            <span className={`${styles.vLine4} d-none d-md-block`}></span>
-            <span className={`${styles.vLine5} d-none d-md-block`}></span>
-            <span className={styles.vLine6}>
-              <Image 
-                src="images/node-bright.svg" 
-                width={15} 
-                height={15} 
-                alt="Circle used for background style"
-                className={styles.node_sm2}
-              />
-            </span>
-            
+
             <nav className="navbar text-center text-md-left">
               <Link className="navbar-brand mx-auto mx-md-4" href="/">
                 <Image 
@@ -69,21 +47,21 @@ class Hero extends Component {
             <div className={`${styles.cover_imgs_container} text-center d-flex justify-content-center animate-slide-up`}>
                 <Image 
                   src="images/phones/device_15_home_feed2.png" 
-                  alt="Screenshot of Roads app showing the main player with several comments."
+                  alt="Screenshot of FitConnect app showing the main player with several comments."
                   height={400}
                   width={200}
                   className='phone_shadow mt-5 d-none d-md-block'
                 />
                 <Image 
                   src="images/phones/device_15_player_full.png" 
-                  alt="Screenshot of Roads app showing the main player with several comments."
+                  alt="Screenshot of FitConnect app showing the main player with several comments."
                   height={500}
                   width={250}
                   className='phone_shadow mx-4'
                 />
                 <Image 
                   src="images/phones/device_15_player_response.png" 
-                  alt="Screenshot of Roads app showing the main player with several comments."
+                  alt="Screenshot of FitConnect app showing the main player with several comments."
                   height={400}
                   width={200}
                   className='phone_shadow mt-5 d-none d-md-block'
